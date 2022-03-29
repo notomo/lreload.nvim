22a9087147c9c0e858e6bfeb88e890b75b422925
notomo <notomo.motono@gmail.com>
2022-03-30 08:37:54 +0900

    Small refactor (option)

--- lua/lreload/command.lua
+++ lua/lreload/command.lua
@@ -26,27 +26,16 @@ local to_group_name = function(name)
   return "lreload_" .. name
 end
 
-function ShowError.enable(name, opts)
-  vim.validate({ name = { name, "string" }, opts = { opts, "table", true } })
-  opts = opts or {}
-
-  vim.validate({ events = { opts.events, "table", true } })
-  opts.events = opts.events or { "BufWritePost" }
-
-  vim.validate({ post_hook = { opts.post_hook, "function", true } })
-
-  local group = vim.api.nvim_create_augroup(to_group_name(name), {})
+function ShowError.enable(name, raw_opts)
+  local opts = require("lreload.option").new(raw_opts)
   vim.api.nvim_create_autocmd(opts.events, {
-    group = group,
+    group = vim.api.nvim_create_augroup(to_group_name(name), {}),
     pattern = { to_pattern(name) },
     callback = function()
       require("lreload").refresh(name)
     end,
   })
-
-  if opts.post_hook then
-    _post_hooks[name] = opts.post_hook
-  end
+  _post_hooks[name] = opts.post_hook
 end
 
 function ShowError.disable(name)
---
+++ lua/lreload/option.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+M.default = {
+  events = { "BufWritePost" },
+  post_hook = function() end,
+}
+
+function M.new(raw_opts)
+  vim.validate({ raw_opts = { raw_opts, "table", true } })
+  raw_opts = raw_opts or {}
+  local opts = vim.tbl_deep_extend("force", M.default, raw_opts)
+  vim.validate({
+    events = { opts.events, "table" },
+    post_hook = { opts.post_hook, "function" },
+  })
+  return opts
+end
+
+return M
