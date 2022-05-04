b9b7cd0e2d10fb04a6a4c3af7484f6c01383a014
notomo <notomo.motono@gmail.com>
2022-05-04 15:24:04 +0900

    Use test helper.cleanup()

--- lua/lreload/test/helper.lua
+++ lua/lreload/test/helper.lua
@@ -1,17 +1,13 @@
 local plugin_name = vim.split((...):gsub("%.", "/"), "/", true)[1]
-local M = require("vusted.helper")
+local helper = require("vusted.helper")
 
-M.root = M.find_plugin_root(plugin_name)
+helper.root = helper.find_plugin_root(plugin_name)
 
-function M.before_each()
-  M.cleanup_loaded_modules(plugin_name)
+function helper.before_each() end
+
+function helper.after_each()
+  helper.cleanup()
+  helper.cleanup_loaded_modules(plugin_name)
 end
 
-function M.after_each()
-  pcall(vim.cmd, "autocmd! lreload")
-  vim.cmd("tabedit")
-  vim.cmd("tabonly!")
-  vim.cmd("silent %bwipeout!")
-end
-
-return M
+return helper
