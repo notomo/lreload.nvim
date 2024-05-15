73268e3e657f758416a88167b9625d1ea66673fd
notomo <notomo.motono@gmail.com>
2024-05-15 21:31:01 +0900

    Small refactor doc gen

--- spec/lua/lreload/doc.lua
+++ spec/lua/lreload/doc.lua
@@ -37,9 +37,7 @@ require("genvdoc").generate(full_plugin_name, {
 })
 
 local gen_readme = function()
-  local f = io.open(example_path, "r")
-  local exmaple = f:read("*a")
-  f:close()
+  local exmaple = util.read_all(example_path)
 
   local content = ([[
 # %s
@@ -51,8 +49,6 @@ Hot-reloading manager for neovim lua plugin development
 ```lua
 %s```]]):format(full_plugin_name, exmaple)
 
-  local readme = io.open("README.md", "w")
-  readme:write(content)
-  readme:close()
+  util.write("README.md", content)
 end
 gen_readme()
